556279d4949a38bd3d482349a598b48aa042c53a
Obunga <61637902+kash-mp4@users.noreply.github.com>
2020-12-16 12:47:23 +0100

    Update Projet_tp10.js

--- TP10_PROJET/js/Projet_tp10.js
+++ TP10_PROJET/js/Projet_tp10.js
@@ -1,5 +1,32 @@
 let naissances = [];
 
+function initialiserTab() {
+    let passages = new Object();
+    document.getElementsByTagName('section')[1].innerHTML = '<table><thead><tr><th>Identité</th><th>Date</th><th>Sexe</th></tr></thead><tbody id="naissances"></tbody></table>'
+}
+
+
+/**
+ * Fonction qui lorsqu'elle est appelée, permet de générer ma Table dans mon HTML
+ *
+ * 
+ * 
+ */
+
+function genereMaTable() {
+    let a = ""
+    for (let i in naissances){
+        a += "<tr><td>" + naissances[i].Nom +" "+ naissances[i].Prenom + "</td><td>" + naissances[i].Date + "</td><td>" + naissances[i].Sexe + "</td></tr>" 
+    }
+    document.getElementById('naissances').innerHTML = a
+}
+
+/**
+ * Fonction qui lorsqu'elle est appelée, récupere toute les valeurers encodées dans mon code HTML
+ * @param {*} formulaire : le nom du formulaire a récuperer
+ *  
+ */
+
 function ajouterNaissance(formulaire) {
 
     var nouveaune = new Object();
@@ -11,6 +38,8 @@ function ajouterNaissance(formulaire) {
     nouveaune['Desc'] = document.getElementById("aujouterDesc").value;
     
     naissances.push(nouveaune);
+
+    genereMaTable()
 
     return false
 }
